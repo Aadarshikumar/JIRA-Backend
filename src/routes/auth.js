c6ef76ec335062f210b42293916f25aa7eb11d9a
Aadarsh <aadarsh21@navgurukul.org>
2025-01-22 11:10:39 +0530

    added the success msg in the response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const AuthPresenter = require('../presenters/AuthPresenter');
-
 const router = express.Router();
-
 router.post('/register', async (req, res) => {
   // {
   //   "username": "aadii",
   //   "password": "123456",
   //   "role": "admin"
   // }
-  
   try {
     const result = await AuthPresenter.register(req.body);
-    res.status(201).json(result);
+    // res.status(201).json(result);
+    res.json({ success: true, message: 'User registered successfully', data: result });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    // res.status(400).json({ error: error.message });
+    res.status(401).json({ success: false, error: error.message });
   }
 });
-
 router.post('/login', async (req, res) => {
   // {
   //   "username": "aadii",
   //   "password": "12345"
   // }
-  
   try {
     const result = await AuthPresenter.login(req.body);
     res.json(result);
+    res.json({ success: true, data: result });
   } catch (error) {
-    res.status(401).json({ error: error.message });
+    // res.status(401).json({ error: error.message });
+    res.status(401).json({ success: false, error: error.message });
   }
 });
-
 module.exports = router;
